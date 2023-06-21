b40c3836c13df84e0927ed8cce11f0b63ca315d8
Felipe Malengo <felipemalengo1@hotmail.com>
2023-06-20 22:52:37 -0300

    att

---
+++ script.js
@@ -0,0 +1,19 @@
+const botaoSalvar = document.querySelector("#btnsalvar");
+const titulo = document.getElementById("#title");
+const sinopse = document.getElementById("#sinopse");
+const modelo = document.getElementById("#model");
+const faixa = document.getElementById("#faixa");
+const genero = document.getElementById("#gen");
+const lista = document.querySelector("#lista");
+
+
+botaoSalvar.addEventListener("click", function() {
+    
+
+    h = document.createElement("h1");
+    
+   lista.appendChild(h)
+   console.log("criado");
+
+  });
+
